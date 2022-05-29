1165c0de84e0052efe38774acf68985412702cbf
Lynda Foster <lfost42@wgu.edu>
2022-05-29 16:31:35 -0700

    edit

--- MyCode/Markdown-Blog/models/article.js
+++ MyCode/Markdown-Blog/models/article.js
@@ -1,6 +1,9 @@
 const mongoose = require('mongoose')
 const marked = require('marked')
 const slugify = require('slugify')
+const createDomPurify = require('dompurify')
+const { JSDOM } = require('jsdom')
+const dompurify = createDomPurify(new JSDOM().window)
 
 const articleSchema = new mongoose.Schema({
 	title: {
@@ -22,15 +25,25 @@ const articleSchema = new mongoose.Schema({
 		type: String,
 		required: true,
 		unique: true
+	},
+	sanitizedHtml: {
+		type: String,
+		required: true
 	}
 })
 
-articleSchema.pre('validate', function(next) {
+articleSchema.pre('validate', function (next) {
 	if (this.title) {
 		this.slug = slugify(this.title, {
 			lower: true,
-			strict: true })
+			strict: true
+		})
 	}
+
+	if (this.markdown) {
+		this.sanitizedHtml = dompurify.sanitize(marked.parse(this.markdown))
+	}
+
 	next()
 })
 
--- MyCode/Markdown-Blog/server.js
+++ MyCode/Markdown-Blog/server.js
@@ -2,16 +2,18 @@ const express = require('express')
 const mongoose = require('mongoose')
 const Article = require('./models/article')
 const articleRouter = require('./routes/articles')
+const methodOverride = require('method-override')
 const app = express()
 
 mongoose.connect('mongodb://localhost/blog', {
 	useNewUrlParser: true,
-	useUnifiedTopology: true
+	useUnifiedTopology: true,
 })
 
 app.set('view engine', 'ejs')
 
 app.use(express.urlencoded({ extended: false }))
+app.use(methodOverride('_method'))
 
 app.get('/', async (req,res) => {
 	const articles = await Article.find().sort({
