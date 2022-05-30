b92746defd0bd3f1d028da8f4ab7de0bda93ed23
Lynda Foster <lfost42@wgu.edu>
2022-05-29 21:33:18 -0700

    bad crud

--- MyCode/firstCRUD/server.js
+++ MyCode/firstCRUD/server.js
@@ -12,12 +12,12 @@ MongoClient.connect(connectionString, { useUnifiedTopology: true })
 
 		app.set('view engine', 'ejs')
 		app.use(bodyParser.urlencoded({ extended: true }))
-
+		app.use(bodyParser.json())
+		app.use(express.static('public'))
 		app.get('/', (req, res) => {
-			res.render('index')
-			db.collection('quotes').find().toArray()
-				.then(quotes => {
-					res.render('index.ejs', { quotes: quotes })
+			quotesCollection.find().toArray()
+				.then(results => {
+					res.render('index.ejs', { quotes: results })
 				})
 				.catch(error => console.error(error))
 		})
@@ -28,6 +28,36 @@ MongoClient.connect(connectionString, { useUnifiedTopology: true })
 				})
 				.catch(error => console.error(error))
 		})
+		app.put('/quotes', (req, res) => {
+			quotesCollection.findOneAndUpdate(
+				{ name: 'Yoda' },
+				{
+					$set: {
+						name: req.body.name,
+						quote: req.body.quote
+					}
+				},
+				{
+					upsert: true
+				}
+			)
+				.then(result => res.json('Success'))
+				.catch(error => console.error(error))
+		})
+
+		app.delete('/quotes', (req, res) => {
+			quotesCollection.deleteOne(
+				{ name: req.body.name }
+			)
+				.then(result => {
+					if (result.deletedCount === 0) {
+						return res.json('No quote to delete')
+					}
+					res.json('Deleted Darth Vader\'s quote')
+				})
+				.catch(error => console.error(error))
+		})
+
 		app.listen(3000, function () {
 			console.log('listening on 3000')
 		})
